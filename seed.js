ef9eb3c4e6a943d52e945930c04f1f5ef19c955a
mertnacakgedigi <mert.nacakgedigi@gmail.com>
2020-03-24 11:40:39 -0700

    Adding new books

---
+++ seed.js
@@ -0,0 +1,29 @@
+db = require('./models');
+
+const books = [
+    {
+    title : "Crime and Punishment",
+    author: "Fyodor Dostoevsky",
+    isbn : "9788897572657",
+    genre:  "Philosophical fiction",
+    ratings: [7,9],
+    image: 'https://images-na.ssl-images-amazon.com/images/I/71CcO-jvRUL.jpg'
+    },
+    {
+    title : "1984",
+    author: "George Orwell",
+    isbn : "9781471331435",
+    genre:  "Classics",
+    ratings: [3,8],
+    image: 'https://images-na.ssl-images-amazon.com/images/I/41E9Z5XaHcL.jpg'
+    },
+    {
+     title: "The Little Prince",
+     author : " Antoine de Saint-Exupéry",
+     isbn: "9791187192596",
+     genre:  "Children",
+     ratings: [8,10],
+     image: 'https://g.christianbook.com/dg/product/cbd/f400/012197.jpg'
+    },
+]
+
