1bfa1bcad5f897effba37e301d51d2f3e1363544
mertnacakgedigi <mert.nacakgedigi@gmail.com>
2020-03-24 13:44:18 -0700

    Checking for seed.js

---
+++ docs/seed_data.js
@@ -0,0 +1,15 @@
+module.exports = {
+    users: [
+    {
+      email: 'seannyphoenix@gmail.com'
+    },
+    {
+      email: 'mert.nacakgedigi@gmail.com'
+    }],
+    books: [
+  
+    ],
+    ratings: [
+  
+    ]
+  }
--- seed.js
+++ seed.js
@@ -1,29 +1,19 @@
-db = require('./models');
+const mongoose = require('mongoose');
+const db = require('./models');
+const data = require('./docs/seed_data.js');
 
-const books = [
-    {
-    title :"Crime and Punishment",
-    author:"Fyodor Dostoevsky",
-    isbn :"9788897572657",
-    genre:"Philosophical fiction",
-    ratings:[7,9],
-    image:'https://images-na.ssl-images-amazon.com/images/I/71CcO-jvRUL.jpg'
-    },
-    {
-    title :"1984",
-    author:"George Orwell",
-    isbn :"9781471331435",
-    genre:"Classics",
-    ratings:[3,8],
-    image:'https://images-na.ssl-images-amazon.com/images/I/41E9Z5XaHcL.jpg'
-    },
-    {
-     title:"The Little Prince",
-     author :" Antoine de Saint-Exupéry",
-     isbn:"9791187192596",
-     genre:"Children",
-     ratings:[8,10],
-     image:'https://g.christianbook.com/dg/product/cbd/f400/012197.jpg'
-    },
-]
+async function seed()
+{
+  try
+  {
+    let info = await db.User.create(data.users);
+    console.log(info);
+  }
+  catch (err)
+  {
+    console.log(err);
+  }
+  mongoose.disconnect();
+}
 
+seed();
