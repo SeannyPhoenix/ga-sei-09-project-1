bea14c2ef9623ce4663ff9e8378f087728d266cd
mertnacakgedigi <mert.nacakgedigi@gmail.com>
2020-03-24 11:45:19 -0700

    Removing spaces :D

--- seed.js
+++ seed.js
@@ -2,28 +2,28 @@ db = require('./models');
 
 const books = [
     {
-    title : "Crime and Punishment",
-    author: "Fyodor Dostoevsky",
-    isbn : "9788897572657",
-    genre:  "Philosophical fiction",
-    ratings: [7,9],
-    image: 'https://images-na.ssl-images-amazon.com/images/I/71CcO-jvRUL.jpg'
+    title :"Crime and Punishment",
+    author:"Fyodor Dostoevsky",
+    isbn :"9788897572657",
+    genre:"Philosophical fiction",
+    ratings:[7,9],
+    image:'https://images-na.ssl-images-amazon.com/images/I/71CcO-jvRUL.jpg'
     },
     {
-    title : "1984",
-    author: "George Orwell",
-    isbn : "9781471331435",
-    genre:  "Classics",
-    ratings: [3,8],
-    image: 'https://images-na.ssl-images-amazon.com/images/I/41E9Z5XaHcL.jpg'
+    title :"1984",
+    author:"George Orwell",
+    isbn :"9781471331435",
+    genre:"Classics",
+    ratings:[3,8],
+    image:'https://images-na.ssl-images-amazon.com/images/I/41E9Z5XaHcL.jpg'
     },
     {
-     title: "The Little Prince",
-     author : " Antoine de Saint-Exupéry",
-     isbn: "9791187192596",
-     genre:  "Children",
-     ratings: [8,10],
-     image: 'https://g.christianbook.com/dg/product/cbd/f400/012197.jpg'
+     title:"The Little Prince",
+     author :" Antoine de Saint-Exupéry",
+     isbn:"9791187192596",
+     genre:"Children",
+     ratings:[8,10],
+     image:'https://g.christianbook.com/dg/product/cbd/f400/012197.jpg'
     },
 ]
 
