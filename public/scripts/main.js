f51a814fe55e57f95e67ee2889b883214079e989
mertnacakgedigi <mert.nacakgedigi@gmail.com>
2020-03-25 16:00:35 -0700

    Get rating is working

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -12,24 +12,43 @@ function createElement(element) {
     let title = element.title;
     let author = element.author;
     let id = element._id;
-    let ratingId = element.ratings;
+
+    let bookLine;
+    if (element.ratings.length){
+      let ratingEl = element.ratings[0].rating;
+      bookLine = ` 
+      <div class="col col-lg-8">
+        ${title} (${author})
+      </div>
+    
+      <div class="col col-lg-2">
+         <i class="fas fa-star">${ratingEl}</i>
+      </div>
+      <div class="col col-lg-2">
+        <i class="fas fa-star"></i>
+      </div>
+   
+    `
+    } else {
+        bookLine = ` 
+        <div class="col col-lg-8">
+          ${title} (${author})
+        </div>
+      
+        <div class="col col-lg-2">
+           <i class="fas fa-star"></i>
+        </div>
+        <div class="col col-lg-2">
+          <i class="fas fa-star"></i>
+        </div>
+     
+      `
+    }
 
     let d1 = document.getElementById('lister');
-    d1.insertAdjacentHTML('beforeend', 
-    
- ` 
-    <div class="col col-lg-8">
-      ${title} (${author})
-    </div>
-  
-    <div class="col col-lg-2">
-       <i class="fas fa-star">${rating}</i>
-    </div>
-    <div class="col col-lg-2">
-      <i class="fas fa-star"></i>
-    </div>
- 
-  `);
+
+        d1.insertAdjacentHTML('beforeend', bookLine);
+   
 
 
 }
