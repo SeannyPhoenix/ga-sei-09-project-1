197aa3f2bc61b11a57ebb44631cff2a90f8b08c0
Seanny Phoenix <seannyphoenix@gmail.com>
2020-03-26 22:51:44 -0700

    Register User Works

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,27 +1,38 @@
 const db = require('../models');
 const bcrypt = require('bcryptjs');
+const emailValidator = require('email-validator');
+const passwordValidator = require('password-validator');
+
+const passwordSchema = new passwordValidator();
+passwordSchema
+  .is().min(8)
+  .is().max(20)
+  .has().uppercase()
+  .has().lowercase()
+  .has().digits()
+  .has().not().spaces();
 
 async function register(req, res) {
   try {
-    // Validate input
-    if (
-      req.firstName === '' ||
-      req.lastName === '' ||
-      req.email === '' ||
-      req.password === ''
-    ) {
+    // Validate the data
+    const validate = [];
+    validate.push(req.body.firstName !== '');
+    validate.push(req.body.lastName !== '');
+    validate.push(emailValidator.validate(req.body.email));
+    validate.push(passwordSchema.validate(req.body.password));
+    if (validate.some(check => !check)) {
       res.json({
-        status: 200,
+        status: 400,
         message: 'Invalid Data'
       });
       return;
     }
 
     // Check if a user with this email address already exists
-    if (await db.User.findOne({
-        email: req.email
-      })) {
-      // Found a user
+    let exists = await db.User.findOne({
+      email: req.body.email
+    });
+    if (exists) {
       res.json({
         status: 400,
         message: 'Account already registered.'
@@ -30,51 +41,33 @@ async function register(req, res) {
     }
 
     // Salt and hash the password
-    // Generate Salt
-    bcrypt.genSalt(10, (err, salt) => {
-      if (err) return res.status(400).json({
-        status: 400,
-        message: 'Something went wrong, please try again'
-      });
-      // Hash User Password
-      bcrypt.hash(req.body.password, salt, (err, hash) => {
-        if (err) return res.status(400).json({
-          status: 400,
-          message: 'Something went wrong, please try again'
-        });
-        // Construct User Object with Hashed Password
-        const userData = {
-          firstName: req.body.firstName,
-          lastName: req.body.lastName,
-          email: req.body.email,
-          password: hash,
-        }
-        // Create New user
-        db.User.create(userData, (err, newUser) => {
-          if (err) return res.status(400).json({
-            status: 400,
-            message: 'Something went wrong, please try again'
-          });
-          res.status(201).json({
-            status: 201,
-            message: 'Success'
-          });
-        });
-      });
-    });
+    let salt = await bcrypt.genSalt(10);
+    let hash = await bcrypt.hash(req.body.password, salt);
+    const userData = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: hash,
+    }
 
-    res.json({
-      status: 200,
-      data: req.body
+    // Create New user
+    let newUser = await db.User.create(userData)
+    res.status(201).json({
+      status: 201,
+      newUser: {
+        id: newUser._id,
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        email: newUser.email
+      }
     });
   }
   catch (err) {
     console.log(`Error`, err);
     res.json({
-      status: 500
+      status: 400
     });
   }
-
 }
 
 async function login(req, res) {
--- models/User.js
+++ models/User.js
@@ -16,23 +16,10 @@ const UserSchema = new Schema({
     type: String,
     required: true,
     unique: true,
-    validate: {
-      validator: function(v) {
-        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
-      },
-      message: props => `${props.value} is not a valid email address`
-    },
   },
   password: {
     type: String,
     required: true,
-    minlength: 4,
-    validate: {
-      validator: (password) => {
-        return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/.test(password);
-      },
-      message: `password does not meet requirements`,
-    },
   },
   ratings: [{
     type: ObjectId,
